4a7dbec551512e8ae9c054c911265b200b8ec5d5
Efim Polevoi <fimkap@Efims-MacBook-Pro-2.local>
2019-03-25 18:34:04 +0300

    Change IP to the server deployed in GC.

--- analytics.js
+++ analytics.js
@@ -1,26 +1,49 @@
+/**
+ * Send analytics data.
+ *
+ * @param {dic} data POST request body
+ * @param {string} mode Type of statistic
+ */
 function sendAnalytics(data, mode) {
     var client_id = getCookie("saCookie");
     data.clientid = `${client_id}`;
-    $.post('http://localhost:5000/analytics?mode='+mode, data, function() {
+    $.post('http://35.230.142.130/analytics?mode='+mode, data, function() {
     });
 }
+
+/**
+ * Create UUID
+ *
+ */
 function createUUID() {
     var s = [];
     var hexDigits = "0123456789abcdef";
     for (var i = 0; i < 36; i++) {
         s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
     }
-    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
-    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[14] = "4";
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);
     s[8] = s[13] = s[18] = s[23] = "-";
     return s.join("");
 }
+
+/**
+ * Create cookie with expiration
+ *
+ * @param {int} daysToExpire Number of days to expire
+ */
 function createCookie(daysToExpire)
 {
     var date = new Date();
     date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
     document.cookie = "saCookie" + "=" + createUUID() + "; expires=" + date.toGMTString();
 }
+
+/**
+ * Get cookie by name
+ *
+ * @param {string} name Name of cookie
+ */
 function getCookie(name) {
     let cookie = {};
     document.cookie.split(';').forEach(function(el) {
@@ -29,36 +52,40 @@ function getCookie(name) {
     })
     return cookie[name];
 }
+
+// If our cookie is not found, create one.
 if (getCookie("saCookie") == undefined) {
     createCookie(5);
 }
+// Get query params.
 var query_params = window.location.search;
 // The smallest meaningful length
 if (query_params.length > 3) {
-    console.log(query_params.substring(1));
     var data={
         query: query_params.substring(1)
     }
     sendAnalytics(data, "query");
 }
-console.log(getCookie("saCookie"));
-console.log(`${location.pathname}`);
+
+// Send page view.
 var data={
     path: `${location.pathname}`
 }
 sendAnalytics(data, "pageview");
 
-// Store mouse moves here
+// Store mouse moves here to send later in batches.
 var moves = [];
 document.addEventListener('mousemove', function (event) {
-    //console.log("mousemove");
     moves.push(event.pageX+','+event.pageY);
 }, false);
 
+/**
+ * Send mouse moves periodically.
+ *
+ */
 function sendMoves() {
     if (moves.length > 0) {
         var movesToSend = moves.join(';');
-        console.log(movesToSend);
         moves = []; // reset
         var data={
             moves: movesToSend
@@ -68,16 +95,16 @@ function sendMoves() {
 }
 setInterval(sendMoves, 5000);
 
+// Add listener for click events.
 document.addEventListener('click', function (event) {
-    console.log(event.target);
     var data={
         target: `${event.target}`
     }
     sendAnalytics(data, "click");
 }, false);
 
+// Add listener for textarea changes.
 document.addEventListener('change', function (event) {
-    console.log("on change: " + event.target.value);
     var data={
         text: `${event.target.value}`
     }
